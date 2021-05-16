b5587de9c210da0861ea13646d5eada111011a3c
small <small0912@gmaul.com>
2021-05-16 17:04:07 +0800

    add cc

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+package xxx;
+
+public class HelloEclipse {
+	
+	private String aa;
+	private String bb;
+	private String cc;
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!!");
+	}
+	
+	//單行註解
+	
+	/*
+	 多行註解
+	 */
+	
+}
